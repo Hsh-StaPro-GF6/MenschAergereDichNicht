7aa7f38e1ca4567429b3a9f5f42aee26d8776153
Marcus Wichelmann <marcus.wichelmann@hotmail.de>
2017-10-10 10:29:48 +0200

    Initial commit

---
+++ GameBoard.java
@@ -0,0 +1,14 @@
+import greenfoot.*;
+
+/**
+ * The main game board.
+ */
+public class GameBoard extends World {
+    /**
+     * Constructs a new game board.
+     * Should only by called once in the game.
+     */
+    public GameBoard() {
+        super(600, 400, 1);
+    }
+}
