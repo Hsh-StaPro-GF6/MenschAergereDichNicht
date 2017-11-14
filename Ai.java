f4acb5d214c56645da89cfc2ea0d2c245e8bd876
EngelbertsTimo <32669417+EngelbertsTimo@users.noreply.github.com>
2017-11-14 08:39:27 +0100

    Add files via upload
    Methoden mit den die Ki arbeiten soll erstellt

---
+++ Ai.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import greenfoot.*;
+
+/**
+ * Alle Ki Handlungen.
+ */
+public class Ai {
+    private final Map map;
+    private final int playerId;
+    private final int start;
+    private final int end;
+    private final Figure[] figures;
+
+    /**
+     * 
+     */
+    public Ai (Map map, int playerId, int start, int end) {
+        
+    }
+
+    private int AvoidSpawn() {
+    	
+    }
+    
+    private int SpawnCamping() {
+    	
+    }
+    
+    private int Homeboy() {
+    	
+    }
+    
+    private int impactPrevention() {
+    	
+    }
+    
+    private int impactChance() {
+    	
+    }
+
+    private int ensurespacing() {
+    	
+    }
+    
+    private int fuimpactPrevention() {
+    	
+    }
+    
+    private int fuimpactChance() {
+    	
+    }
+    
+    private int leaderHunt() {
+    	
+    }       
+}
