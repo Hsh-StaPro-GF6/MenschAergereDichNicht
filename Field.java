bc8fd01164b5e21f61044e3e8e414947e323a617
Marcus Wichelmann <marcus.wichelmann@hotmail.de>
2017-12-05 12:01:21 +0100

    Bilder ersetzt

--- Field.java
+++ Field.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
 import java.util.*;
 
 /**
@@ -10,12 +11,14 @@ public class Field extends Actor {
 
     // Bild, wenn das Feld leer ist
     private GreenfootImage image;
-    //test
-    private GreenfootImage test;
 
     // Bild, wenn auf dem Feld eine Figur steht
     // Player: 0 = Rot, 1 = Blau, 2 = Grün, 3 = Gelb
     private GreenfootImage[] imageWhenOccupied = new GreenfootImage[4];
+
+    // Bild, wenn auf dem Feld eine Figur steht und diese auswählbar ist
+    // Player: 0 = Rot, 1 = Blau, 2 = Grün, 3 = Gelb
+    private GreenfootImage[] imageWhenOccupiedSelectable = new GreenfootImage[4];
 
     // Bild, wenn das Feld das Startfeld eines Spielers ist
     // Player: 0 = Rot, 1 = Blau, 2 = Grün, 3 = Gelb
@@ -32,10 +35,10 @@ public class Field extends Actor {
      * Feld zeichnen.
      */
     public void act() {
-        
+
         // Feld auf dem eine Figur steht
-        Figure figureAtStreetPosition = gameManager.getMap().getFigureAtStreetPosition(fieldId);        
-        
+        Figure figureAtStreetPosition = gameManager.getMap().getFigureAtStreetPosition(fieldId);
+
         // Prüfen, ob dies das Startfeld eines Spielers ist
         int startFieldOfPlayer = -1;
         switch (fieldId) {
@@ -67,22 +70,16 @@ public class Field extends Actor {
         // Feldstatus darstellen
         int figureOwner = fieldOccupied ? figureAtStreetPosition.getPlayer().getId() : -1;
 
+        // Ist das Feld mit einer bewegbaren Figur belegt && Ist ein Mensch an der Reihe
+        boolean selectable = (gameManager.getCurrentDecision() != null && Arrays.asList(gameManager.getCurrentDecision().getMovableFigures()).contains(figureAtStreetPosition) &&
+                gameManager.getCurrentDecision().getPlayer().getMember() instanceof HumanMember);
+
         // Steht auf dem Feld eine Figur?
         if (fieldOccupied) {
-            this.setImage(imageWhenOccupied[figureOwner]);
-            return;
-        }
-        //
-        //
-        // Ist das Feld mit einer bewegbaren Figur belegt && Ist ein Mensch an der Reihe
-       
-       if (gameManager.getCurrentDecision() != null &&
-       Arrays.asList(gameManager.getCurrentDecision().getMovableFigures()).contains(figureAtStreetPosition) && gameManager.getCurrentDecision().getPlayer().getMember() instanceof HumanMember   ){
-            this.setImage(test);
+            this.setImage(selectable ? imageWhenOccupiedSelectable[figureOwner] : imageWhenOccupied[figureOwner]);
             return;
         }
 
-        
         // Start-Feld eines Spielers?
         if (startFieldOfPlayer != -1) {
             this.setImage(startImage[startFieldOfPlayer]);
@@ -100,10 +97,13 @@ public class Field extends Actor {
         imageWhenOccupied[1] = new GreenfootImage("images/blue22.png");
         imageWhenOccupied[2] = new GreenfootImage("images/green22.png");
         imageWhenOccupied[3] = new GreenfootImage("images/yellow22.png");
+        imageWhenOccupiedSelectable[0] = new GreenfootImage("images/red22_selectable.png");
+        imageWhenOccupiedSelectable[1] = new GreenfootImage("images/blue22_selectable.png");
+        imageWhenOccupiedSelectable[2] = new GreenfootImage("images/green22_selectable.png");
+        imageWhenOccupiedSelectable[3] = new GreenfootImage("images/yellow22_selectable.png");
         startImage[0] = new GreenfootImage("images/RED-Start.png");
         startImage[1] = new GreenfootImage("images/BLUE-Start.png");
         startImage[2] = new GreenfootImage("images/GREEN-Start.png");
         startImage[3] = new GreenfootImage("images/YELLOW-Start.png");
-        test = new GreenfootImage("images/1.png");
     }
 }
