2015516ef254a67e5a75f58d0f9916e72fe05760
Marcus Wichelmann <marcus.wichelmann@hotmail.de>
2017-12-12 17:37:46 +0100

    Gewinn-Animation hinzugefügt

--- PlayerWonDisplay.java
+++ PlayerWonDisplay.java
@@ -1,17 +1,18 @@
 import greenfoot.*;
 
 public class PlayerWonDisplay extends Actor {
-    private GifImage gif = new GifImage("images/PlayerWon.gif");
-    private int countdown = 250;
+    private int imageIndex = 0;
 
     public PlayerWonDisplay(Player wonPlayer) {
 
     }
 
     public void act() {
-        setImage(gif.getCurrentImage());
-
-        if (countdown-- <= 0)
+        if (++imageIndex >= 100) {
             getWorld().removeObject(this);
+            return;
+        }
+
+        setImage(new GreenfootImage("images/PlayerWon_frames/" + "0000".substring((imageIndex + "").length()) + imageIndex + ".png"));
     }
 }
